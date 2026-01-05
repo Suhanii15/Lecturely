55b8815d34805024614fe4454a38869ef8fb3121
Suhanii15 <suhanikabra931@gmail.com>
2026-01-05 11:44:59 +0530

    initialized backend

---
+++ server/lib/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB= async ()=>{
+    try{
+await mongoose.connect(process.env.MONGODB_URI);
+console.log("Database connected")
+    }
+    catch(error){
+console.error(" nhi ho paya connect kyuki", error.messaage)
+    }
+};
+
+module.exports=connectDB
---
+++ server/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const express=require('express');
+const app=express();
+const cors=require('cors');
+
+const http =require('http');
+const server=http.createServer(app); //http server isiliye use kiya kyuki socket.io use karta hau isko
+const connectDB=require('./lib/db');
+
+connectDB(); // database connect karne ke liye function call kiya
+
+app.use(express.json({limit : "10 mb"}));// middleware set up, here limit shows ki itni sie ki image daal sakte hai
+app.use(express.urlencoded({ limit:"10mb", extended: true }));
+
+app.use(cors()); // ye cors ka use isliye kiya taki front end or back end ke beech me communication ho sake, basically URL connection hai dono ka
+
+
+//routes
+app.use("/api/status", (req,res)=> res.send("Server chal raha"));
+
+
+const PORT=process.env.PORT || 5000; // 
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
